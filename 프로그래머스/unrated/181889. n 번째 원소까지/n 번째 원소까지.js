6bd3c7705e0bb163ea413f49139ec5ea25c2e189
Parkhyun <i_am_heeeun@naver.com>
2023-06-21 23:55:37 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -0,0 +1,11 @@
+function solution(num_list, n) {
+    var answer = [];
+    
+    num_list.map((v,i)=>{
+        if(i+1 <= n){
+            answer.push(v);
+        }
+        
+    })
+    return answer;
+}
